1665a7734390cd9a95f564911475172506c88624
lssb2003 <lssb2003@gmail.com>
2024-12-04 23:27:26 +0800

    your commit message

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,9 +2,11 @@ ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
 
-module ActiveSupport
-  class TestCase
-    parallelize(workers: :number_of_processors)
-    fixtures :all
+Rails.application.credentials.stub :secret_key_base, "test_secret" do
+  module ActiveSupport
+    class TestCase
+      parallelize(workers: :number_of_processors)
+      fixtures :all
+    end
   end
 end
