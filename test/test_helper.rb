6b895d88eeb1bd03f27a35dc7077b0ac72dfd271
lssb2003 <lssb2003@gmail.com>
2024-12-04 22:53:13 +0800

    your commit message

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,14 +2,11 @@ ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
 
+Rails.application.credentials.secret_key_base = "test_secret_key" unless Rails.env.production?
+
 module ActiveSupport
   class TestCase
-    # Run tests in parallel with specified workers
     parallelize(workers: :number_of_processors)
-
-    # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
     fixtures :all
-
-    # Add more helper methods to be used by all tests here...
   end
 end
